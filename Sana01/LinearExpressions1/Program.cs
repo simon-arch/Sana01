f0c650cdd7e00dd282002a7bda0e0e9d88722c6a
Артем Ващенко <ipz231_vai@student.ztu.edu.ua>
2023-11-06 17:25:38 +0200

    First task finished

--- Sana01/LinearExpressions1/Program.cs
+++ Sana01/LinearExpressions1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        double a, b, c, d;
+        double x, y, z, r;
+
+        while (true) {Console.Write("A: ");
+            if (double.TryParse(Console.ReadLine(), out a)) break;
+            else Console.WriteLine("Wrong input.");}
+        
+        while (true) {Console.Write("B: ");
+            if (double.TryParse(Console.ReadLine(), out b)) break;
+            else Console.WriteLine("Wrong input.");}
+
+        while (true) {Console.Write("C: ");
+            if (double.TryParse(Console.ReadLine(), out c)) break;
+            else Console.WriteLine("Wrong input.");}
+
+        while (true) {Console.Write("D: ");
+            if (double.TryParse(Console.ReadLine(), out d)) break;
+            else Console.WriteLine("Wrong input.");}
+
+        x = (a + 2 * b - c + d) / c * d + (a + b) / (c - d) - Math.Pow(a, 2) / Math.Pow(b, 2);
+        y = 5 * (a + b) * (c - d) / (1 / 2.0 * c) + (Math.Pow(d, 2)) * ((Math.Pow(a, 2) - Math.Pow(b, 2)) / (b - a));
+        z = (Math.Pow(Math.Pow(x, 2) - 2 * x, 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b) / (5 * a + 3 * b);
+        r = (1 / 2.0 * a + 3 / 4.0 * b - 7 / 5.0) / (3 * c + 1) + (1 / (a - c));
+
+        Console.Write("\nx = "); Console.WriteLine(x);
+        Console.Write("y = "); Console.WriteLine(y);
+        Console.Write("z = "); Console.WriteLine(z);
+        Console.Write("r = "); Console.WriteLine(r);
+    }
+}
